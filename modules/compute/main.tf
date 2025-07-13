091a4ddc59ac72470943b397a0d987c9077b1d22
lokesh dutta <89760497+lokkesshhh@users.noreply.github.com>
2025-07-13 19:30:53 -0400

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,3 +1,16 @@
+#     # The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+    #         # The name of your Terraform Cloud organization.
+    organization = "GCP_lokesh"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "GCP_tf"
+    }
+  }
+}
+
 provider "google" {
   project = "phonic-presence-310804"
   region  = var.region
