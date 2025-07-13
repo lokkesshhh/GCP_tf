ef42d633bff6163d19a22578dec575674f6cc335
lokesh dutta <89760497+lokkesshhh@users.noreply.github.com>
2025-07-13 19:24:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 #     # The configuration for the `remote` backend.
-     terraform {
-       backend "remote" {
-#         # The name of your Terraform Cloud organization.
-         organization = "GCP_lokesh"
+terraform {
+  backend "remote" {
+    #         # The name of your Terraform Cloud organization.
+    organization = "GCP_lokesh"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "GCP_tf"
-         }
-       }
-     }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "GCP_tf"
+    }
+  }
+}
